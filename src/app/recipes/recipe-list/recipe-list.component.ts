845289f153bdc384de04087cf2b7cdffdc62f7b8
yue <yue@yues-MacBook-Pro.local>
2017-12-06 20:50:20 -0500

    out put hard code recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test Recipe','a test recipe','https://eat24hours.com/files/cuisines/v4/thai.jpg?e24v=103?e24v=178?e24v=178'),
+    new Recipe('test Recipe','a test recipe','https://eat24hours.com/files/cuisines/v4/thai.jpg?e24v=103?e24v=178?e24v=178'),
+  
+  ]
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,6 +5,7 @@ export class Recipe {
     
     constructor(name: string, desc: string, imagePath: string) {
         this.name = name;
+        this.description = desc;
         this.imagePath = imagePath;   
     }
 } 
