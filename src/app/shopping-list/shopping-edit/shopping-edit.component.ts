142c048f571169b0b945d135e9a6a38bb2ec8f7f
greenchoicenow <song1991yue@gmail.com>
2017-12-13 12:52:49 -0500

    shopping list addable

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+   OnInit, 
+   ViewChild, 
+   ElementRef, 
+   Output, 
+   EventEmitter } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() IngredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddShoppingList() {
+    console.log('add');
+    console.log(this.nameInput);
+    console.log(this.amountInput);
+    this.IngredientAdded.emit({
+      name: this.nameInput.nativeElement.value,
+      amount: this.amountInput.nativeElement.value,
+      unit:''
+    });
+  }
+
+  onDeleteShoppingList() {
+    console.log('de');
+  }
+
+  onClearShoppingList() {
+    console.log('clear');
+  }
 }
