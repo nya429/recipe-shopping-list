e96826a7cc4711197a492875869515a11a73995a
greenchoicenow <song1991yue@gmail.com>
2017-12-15 12:13:32 -0500

    add dropwdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,9 +4,11 @@ import { Directive, OnInit, HostListener, HostBinding, ElementRef, Renderer2 } f
     selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit {
-    @HostBinding()
-
-    @HostListener()
+    @HostBinding('class.open') isOpen = false;
+    
+    @HostListener('click') toggleOpen() {
+        this.isOpen = !this.isOpen;
+    }
 
     constructor(private elRef: ElementRef, private renderer: Renderer2) { }
     ngOnInit() {
